1b7347ed600a78ee97d74f3282c08a096b5c092f
Abdullah <abdullahkhan221121@gmail.com>
2024-01-10 18:18:18 +0500

    new

---
+++ one.js
@@ -0,0 +1,18 @@
+var organization = [
+
+    {
+        "id" : 0 ,
+        "name" : "abc" ,
+        "designation" : "Manager" ,
+        "salary" : 50000 ,
+        "phone-no" : 1234567 ,
+    },
+
+    {
+        "id" : 0 ,
+        "name" : "xyz" ,
+        "designation" : "General-Manager" ,
+        "salary" : 500000 ,
+        "phone-no" : 23457 ,
+    },
+]
